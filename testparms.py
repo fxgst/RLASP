5779f6ae8c46859b365c291e92537a0bfb60d4ad
Elias Datler <eliasdatler@me.com>
2020-06-23 14:08:02 +0200

    Fix indentation in testparams.py

--- testparms.py
+++ testparms.py
@@ -1,13 +1,13 @@
 # set test parameters for generating test data
 # results will be saved in /testdata
 
-num_blocks 				= 5 # just for the test name, need to add/remove blocks and subgoals in .lp files under "input predicates"!
-number_episodes 		= 400
-number_runs 			= 10
-planning_factor     	= 0.1
-plan_on_empty_policy	= False
-planning_horizon		= 2
+num_blocks              = 5 # just for the test name, need to add/remove blocks and subgoals in .lp files under "input predicates"!
+number_episodes         = 400
+number_runs             = 10
+planning_factor         = 0.1
+plan_on_empty_policy    = False
+planning_horizon        = 2
 
-max_episode_len			= num_blocks*2
-mode 					= 'mean'
-plot_points 			= 200
+max_episode_len         = num_blocks*2
+mode                    = 'mean'
+plot_points             = 200
