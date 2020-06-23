c25726c69f744357457c750ebf9c3498f2b02714
Elias Datler <eliasdatler@me.com>
2020-06-23 13:38:25 +0200

    Add description to testparams.py

--- testparms.py
+++ testparms.py
@@ -1,7 +1,7 @@
 # set test parameters for generating test data
 # results will be saved in /testdata
 
-num_blocks 				= 5
+num_blocks 				= 5 # just for the test name, need to add/remove blocks and subgoals in .lp files under "input predicates"!
 number_episodes 		= 400
 number_runs 			= 10
 planning_factor     	= 0.1
